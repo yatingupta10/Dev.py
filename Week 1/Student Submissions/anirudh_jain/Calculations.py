8d198b740ebc72b0891f9fe0f45ab09428990db0
Anirudh Jain <Anirudh Jain>
2016-10-08 23:24:53 +0530

    the 3 questions are solved in three new different files

---
+++ Week 1/Student Submissions/anirudh_jain/Calculations.py
@@ -0,0 +1,37 @@
+# Real numbers
+
+a= 2
+b= 4
+
+# 1.Addition
+
+c = a+b
+print(c)
+
+# 2.Subtraction
+
+c =a-b
+print(c)
+
+# 3. Multiplication
+
+c = a*b
+print(c)
+
+# 4.a Division
+
+c = a/b
+print(c)
+
+# 4.b Floating numbers division
+
+a= 91.9
+b= 92.7
+
+c = b/a
+print(c)
+
+# 5. Remainder
+
+c = b%a
+print(c)
---
+++ Week 1/Student Submissions/anirudh_jain/fruits.py
@@ -0,0 +1,10 @@
+a = input('Enter the number of fruits\n')
+b= int(a)
+print (b)
+
+list = []
+
+c = input('Enter the name of fruit')
+
+list.insert(0,c)
+print(list)
