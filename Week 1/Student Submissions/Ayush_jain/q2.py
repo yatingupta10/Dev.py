97913dfcd691f3cdd9b968b1b6c9e1c33ff1ef1f
Ayush <ayushaj007@gmail.com>
2016-10-09 20:55:28 +0530

    first task

---
+++ Week 1/Student Submissions/Ayush_jain/q2.py
@@ -0,0 +1,18 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+print "sum is=		",num1 + num2
+print "difference is=	",num1- num2
+print "product is=	",num1*num2
+print "division is=	",num1/num2
+print "modulus is =	",num1%num2
+
+num3=float(input("Enter the first number(float): "))
+num4=float(input("Enter the second number: "))
+
+print "sum is=",num3 + num4
+print "difference is=",num3- num4
+print "product is=",num3*num4
+print "division is=",num3/num4
+print "modulus is =	",num3%num4
+
