4fc396f886ddcba8637d09508511cf7a6cc52e91
[sahil] <shaneamgarcia@gmail.com>
2016-10-29 09:35:50 +0530

    app

---
+++ Week 4/Student Submissions/sahil/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from.models import Post
+def post_list(request):
+    posts=Post.objects.all()
+    return render(request,'blog\post_list.html',{'posts':posts})
+# Create your views here.
